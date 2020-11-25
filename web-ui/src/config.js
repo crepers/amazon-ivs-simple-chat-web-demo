9e7086494be77d724d329d8d54b367c155f41194
Jung <woochul@909c4aba0131.ant.amazon.com>
2020-11-25 14:21:51 +0900

    update websocket url.

--- web-ui/src/config.js
+++ web-ui/src/config.js
@@ -3,7 +3,8 @@
 
 // Amazon IVS Playback URL
 // Replace this with your own Amazon IVS Playback URL
-export const PLAYBACK_URL = "https://fcc3ddae59ed.us-west-2.playback.live-video.net/api/video/v1/us-west-2.893648527354.channel.DmumNckWFTqz.m3u8";
+//export const PLAYBACK_URL = "https://fcc3ddae59ed.us-west-2.playback.live-video.net/api/video/v1/us-west-2.893648527354.channel.DmumNckWFTqz.m3u8";
+export const PLAYBACK_URL = "https://e42d3a468498.us-east-1.playback.live-video.net/api/video/v1/us-east-1.892090217348.channel.mmpm2go2q69W.m3u8";
 
 // Chat websocket address
-export const CHAT_WEBSOCKET = "";
+export const CHAT_WEBSOCKET = "wss://g7yxkmyafg.execute-api.ap-northeast-2.amazonaws.com/Prod";
